8304f76ce21be8a57610503f5895400918a547fa
Richard Schorrig <richard.schorrig@web.de>
2021-10-10 01:29:43 +0200

    added comments, some refactorings, added reading from template

--- EmailSender.py
+++ EmailSender.py
@@ -19,8 +19,15 @@ class EmailSender:
             """
             self.args = args
 
-    def __init__(self, subject, text, textArgs, *receiverList, test=False, password=None, emailaddress=None, doNotDeleteSentMails=False):
-        """ constructor is used to to all the work of sending the email template
+    def __init__(self, subject, text, textArgs, *, emailaddress=None, password=None, doNotDeleteSentMails=False, test=False):
+        """constructor is used to do all the work of sending the email template
+        --- CAUTION: use a junk gmail account for this, to not lose important emails ---
+        Gmail has to be set up that it allows less secure apps and allow access via IMAP and SMTP
+        the list of receiving emails shall be provided with the textArgs directory.
+        Example: textArg[test@test.com] = TextArg("test", 123)
+        This way the receiver is tied to his personal email message.
+        After the emails have been sent, they are deleted from the server using imap. Be careful, as this function simply
+        searches for the subject. This could affect other emails, too.
         @param subject: the subject of the email
         @param text: a text template. This is formatted using the function 'format' and the given textArgs
         @param textArgs: a directory (key is the receivers email) of arguments to format the text
@@ -29,11 +36,12 @@ class EmailSender:
         @param emailaddress: the emailaddress to use, will be asked if not provided
         @param doNotDeleteSentMails: set this to true if you want to keep the sent emails in the sentbox of your email account
         """
-        if (None == password) & (None == emailaddress) & (not test):
+        if (None == emailaddress):
             # ask for gmail address and password
             print("type your gmail address (only works with gmail) and password")
             print("email address: ", end="")
-            sendMail = input()
+            emailaddress = input()
+        if (None == password) & (not test):
             password = getpass()
         
         smtp_server = EmailSender.smtp_server
@@ -43,6 +51,7 @@ class EmailSender:
         if test:
             smtp_server = "localhost"
             smtp_port = 1025
+
         # send the emails
         with smtplib.SMTP(smtp_server, smtp_port) as server:
             try:
@@ -52,18 +61,18 @@ class EmailSender:
                     server.ehlo()
                     server.starttls(context=context) # and secure the connection
                     server.ehlo()
-                    server.login(sendMail, password)
+                    server.login(emailaddress, password)
 
                 message = EmailMessage()
-                message['from'] = sendMail
+                message['from'] = emailaddress
                 message['subject'] = subject
                 
-                for receiver in receiverList:
+                for receiver in textArgs.keys():
                     del message['to']
                     message['to'] = receiver
                     message.clear_content()
                     message.set_content(text.format(*textArgs[receiver].args))
-                    server.sendmail(sendMail, receiver, message.as_string())
+                    server.sendmail(emailaddress, receiver, message.as_string())
 
             except Exception as e:
                 print(e)
@@ -74,7 +83,7 @@ class EmailSender:
         if (not doNotDeleteSentMails) & (not test):
             with imaplib.IMAP4_SSL(EmailSender.imap_server) as imap:
                 try:
-                    imap.login(sendMail, password)
+                    imap.login(emailaddress, password)
                     print(imap.select('[Gmail]/Gesendet', readonly=False))
                     typ, data = imap.search(None, 'SUBJECT \"' + subject + '\"')
                     for num in data[0].split():
@@ -88,33 +97,34 @@ class EmailSender:
                     imap.logout()
     
 def test():
-
-    # For this test, the build in smtp daemon can be used, run:
-    # python -m smtpd -c DebuggingServer -n localhost:1025
+    """For this test, the build in smtp daemon shall be used, run:
+    python -m smtpd -c DebuggingServer -n localhost:1025
+    in a console / shell
+    """
 
     text = "This is a test for {0}\n" +\
-           "Hello {0},\n" +\
-           "You are won Eur {1},{2:02d}!\n" +\
+           "Hello {0} {1},\n" +\
+           "You are won ${2},{3:02d}!\n" +\
            "\n" +\
            "Yours sincerely\n" +\
-           "Spammer\n"
+           "Newman\n"
     textArgs = {}
-    emailReceiver = ("richard.schorrig@fn.de",
-                      "richard.schorrig@web.de",
-                      "richard.schorrig@outlook.de",
-                      "richi.schorrig@t-online.de")
+    emailReceiver = ("george.costanza@seinfeld.com",
+                     "jerry.seinfeld@seinfeld.com",
+                     "elaine.benes@seinfeld.com",
+                     "cosmo.kramer@seinfeld.com")
 
     import random
     import time
+    import re
 
     random.seed(time.time())
     for receiver in emailReceiver:
-        textArgs[receiver] = EmailSender.TextArgs(("Happy Winner #" + str(random.randint(1, 10000)), random.randint(0,1), random.randint(0,99)))
-    
-    for receiver in emailReceiver:
-        print(text.format(*textArgs[receiver].args))
+        firstName = str.capitalize(re.split("[.@]", receiver)[0])
+        lastName = str.capitalize(re.split("[.@]", receiver)[1])
+        textArgs[receiver] = EmailSender.TextArgs((firstName, lastName, random.randint(0,1), random.randint(0,99)))
 
-    EmailSender("You Won!", text, textArgs, *emailReceiver, test=True)
+    EmailSender("You Win!", text, textArgs, test=True, emailaddress="newman@hawaiianpostaloffice.com")
 
 if __name__ == '__main__':
     test()
--- SecretSanta.py
+++ SecretSanta.py
@@ -1,18 +1,21 @@
 import csv
 from getpass import getpass
+import getopt
+import sys
 
 from PartnerChooser import PartnerChooser
 from EmailSender import EmailSender
 
 class SecretSanta:
 
-    def __init__(self, wishlist):
-
-        csvFile = open(wishlist)
-        csvReader = csv.reader(csvFile, delimiter=";", quotechar="\"")
+    def __init__(self, wishlist, template, *, emailaddress=None, password=None, doNotDeleteSentMails=False, test=False):
 
         emails = {}
         wishes = {}
+
+        # open and parse the csv file
+        csvFile = open(wishlist)
+        csvReader = csv.reader(csvFile, delimiter=";", quotechar="\"")
         for row in csvReader:
             if len(row) < 2:
                 raise Exception("in the csv file, at least the email address and the name is expected, actual: " *row)
@@ -21,7 +24,15 @@ class SecretSanta:
             if len(row) > 2:
                 wishes[row[1]] = row[2]
             else:
-                wishes[row[1]] = "nichts besonderes"
+                wishes[row[1]] = "nothing special"
+        csvFile.close()
+
+        # open and read the email template; the first line is used as the subject
+        templateFile = open(template, "rt")
+        text = templateFile.read()
+        subject = text.splitlines(False)[0]
+        text = "".join(text.splitlines(True)[1:])
+        templateFile.close()
 
         print("now smash your keyboard to generate a random seed")
         seed = getpass()
@@ -33,16 +44,84 @@ class SecretSanta:
         print("with your seed, choosing a partner")
         partnerChooser = PartnerChooser(list(wishes.keys()), seed)
 
-        text = "Hallo {0}\nDein zu Beschenkender heißt {1} und wünscht sich {2}\n"
+        if not partnerChooser.IsSuccessful():
+            print("could not assign all persons with a different partner. Try another seed.")
+            return
+
         textArgs = {}
         for name in emails.keys():
             partner = partnerChooser.GetPartner(name)
             textArgs[emails[name]] = EmailSender.TextArgs((name, partner, wishes[partner]))
 
-        EmailSender("Wichteln", text, textArgs, *list(emails.values()))
+        EmailSender(subject, text, textArgs, emailaddress=emailaddress, password=password, doNotDeleteSentMails=doNotDeleteSentMails, test=test)
 
-def test():
-    SecretSanta("test3.csv")
+def printHelp():
+    help(main)
 
-if __name__ == "__main__":
-    test()
+def main(argv):
+    """Secret Santa partner chooser and email sender
+    this script will shuffle a list of names to provide each person with a partner and send
+    emails out to inform the person which partner was assigned (and an optional wish)
+    Arguments: <path to a csv file> <path to a text file>
+    --email=<email address of the sender (gmail only)> --password<password of the email account>
+    --keepEmails --test
+    positional arguments:
+    the first argument is the path to a csv file where all attendees are listed. The first column
+    is the email address, the second is the name, and in the third column a wish can be provided
+    Example:
+    --randymarsh@southpark.com;Randy Marsh;Beer--
+    the second argument is the email template. Use the spaceholders {0}, {1} and {2} to provide
+    the gaps where the values from the csv are filled in. The first line is used as the subject.
+    {0} is the name of the receiver
+    {1} is the name of the partner
+    {2} is the wish
+    Example:
+    --Secret Santa--
+    --Hello {0}, your secret santa is {1} and wishes for {2}--
+    
+    keyword arguments:
+    --email: optionally provide the email address for a gmail account (use a throwaway account)
+    --password: the password for the account
+            if no credentials are provided, they can be put in later
+    --keepEmails: this keeps the emails in your email account's sent folder, so you can see who
+            got who
+    --test: use this parameter and the smtp daemon to see the result of your email, without
+            actually sending them
+    """
+    if len(argv) < 3:
+        printHelp()
+        print("\nnot enough arguments provided, exiting...")
+        return
+
+    csvFile = argv[1]
+    templateFile = argv[2]
+    emailAddress = None
+    password = None
+    keepEmails = False
+    test = False
+
+    try:
+        opts, args = getopt.getopt(argv[3:], "h", ["email=", "password=", "keepEmails", "test"])
+    except getopt.GetoptError as error:
+        printHelp()
+        print(error)
+        return
+
+    for opt, arg in opts:
+        if "--email" == opt:
+            emailAddress = arg
+        elif "--password" == opt:
+            password = arg
+        elif "--keepEmails" == opt:
+            keepEmails = True
+        elif "--test" == opt:
+            test = True
+        elif "-h" == opt:
+            printHelp()
+        else:
+            print("unknown argument " + arg)
+
+    SecretSanta(csvFile, templateFile, emailaddress=emailAddress, password=password, doNotDeleteSentMails=keepEmails, test=test)
+
+if __name__ == '__main__':
+    main(sys.argv)
